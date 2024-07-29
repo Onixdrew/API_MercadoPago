c529b50f77fcf9613ff5b81cf48fd22ff9a9e673
Onixdrew <andrewmosquera1811@gmail.com>
2024-07-29 12:32:54 -0500

    'Actualizado'

--- src/routes/Productos.js
+++ src/routes/Productos.js
@@ -13,7 +13,7 @@ router.post("/agregarProducto", (req, res) => {
 
 // obtener Productos
 
-router.get("/obtenerProductos", (req, res) => {
+router.get("/c", (req, res) => {
   productoSchema
     .find()
     .then((dato) => res.json(dato))
