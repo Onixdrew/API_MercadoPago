db6b6bd540d82ceb673d20b5aa1550c31eed48f0
Santiago Mosquera <132929561+Onixdrew@users.noreply.github.com>
2024-07-31 12:24:58 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,3 +22,38 @@ mongoose
   .catch(() => console.error("error al conectar con mongo atlas"));
 
 app.listen(port, () => console.log("servidor activado en el puerto", port));
+
+
+
+
+// CORS
+
+// const express = require("express");
+// const mongoose = require("mongoose");
+// require("dotenv").config();
+// const cors = require("cors"); // Importar el paquete cors
+// const RutasProductos = require("./routes/Productos");
+// const RutasUser = require("./routes/Usuarios");
+
+// const app = express();
+// const port = process.env.PORT || 9000;
+
+// // Configurar CORS
+// app.use(cors());
+
+// // Configurar middleware para analizar JSON
+// app.use(express.json());
+// app.use("/api", RutasProductos);
+// app.use("/api", RutasUser);
+
+// app.get("/", (req, res) => {
+//   res.send("Bienvenido al API de MercadoPago");
+// });
+
+// // Conexión a MongoDB
+// mongoose
+//   .connect(process.env.mongodb_url2)
+//   .then(() => console.log("conectado a mongo atlas"))
+//   .catch(() => console.error("error al conectar con mongo atlas"));
+
+// app.listen(port, () => console.log("servidor activado en el puerto", port));
