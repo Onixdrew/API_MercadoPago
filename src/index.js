74d12605b310a479044a52656b1ba66071b7eca8
Santiago Mosquera <132929561+Onixdrew@users.noreply.github.com>
2024-08-08 08:59:23 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,10 +9,8 @@ const port = process.env.PORT || 9000;
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();
 });
 
